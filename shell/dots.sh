60527be4fa3f27aeec89a637d54210c00c74ca22
Dhruv Bhattacharya <81327087+NachoCheez@users.noreply.github.com>
2021-08-09 19:42:40 +0530

    Create dots.sh

---
+++ shell/dots.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/karlstav/cava
+cd cava
+./autogen.sh
+./configure
+make
+sudo make install
+cd
+
+sudo pacman -Syyu git base-devel xorg alacritty i3-gaps dmenu rofi picom redshift nitrogen polybar fftw ncurses alsa-lib iniparser pulseaudio xorg-fonts-misc 
+yay -Syyu siji-git ttf-unifont
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
+
+mkdir dotfiles
+cd dotfiles
+git clone https://github.com/NachoCheez/.config
+
+mkdir ~/.config/alacritty
+cp -r ~/dotfiles/.config/alacritty ~/.config/
+cd
+
+mkdir ~/.config/i3
+cp -r ~/dotfiles/.config/i3 ~/.config/
+cd
+
+mkdir ~/.config/polybar
+cp -r ~/dotfiles/.config/polybar ~/.config
+cd
