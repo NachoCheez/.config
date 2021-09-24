5b76634b1bd8565906886d77e4ac85988e699f87
Dhruv Bhattacharya <81327087+NachoCheez@users.noreply.github.com>
2021-09-24 16:01:22 +0530

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,13 @@
 # kill existing polybars
 killall -q polybar
 # start polybars
-polybar main
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+polybar bar1 &
+polybar bar2 &
+
